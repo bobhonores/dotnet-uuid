d05c2025243ebe837e8cd1a289954ff92a58485d
Roberto Honores <Roberto.Honores@tranzact.net>
2024-10-15 18:09:19 -0500

    feat: refactor and include UuidExtensions

---
+++ src/UUIDTester.Api/DateOnlyTypeHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Dapper;
+
+public class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
+{
+    public override void SetValue(IDbDataParameter parameter, DateOnly value)
+    {
+        parameter.DbType = DbType.Date;
+        parameter.Value = value;
+    }
+
+    public override DateOnly Parse(object value)
+    {
+        return DateOnly.FromDateTime((DateTime)value);
+    }
+}
